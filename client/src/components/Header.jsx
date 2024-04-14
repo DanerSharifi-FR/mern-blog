9cdc708ceb19ea77b7532eec3e19a39a9298c7b7
Daner SHARIFI <daner.sharifi.official@gmail.com>
2024-04-14 02:56:29 +0200

    Create and run the server
    Using express

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = function (props) {
                     <FaMoon/>
                 </Button>
                 <Link to='/sign-in'>
-                    <Button gradientDuoTone="purpleToBlue" pill>
+                    <Button gradientDuoTone="purpleToBlue" pill outline>
                         Sign In
                     </Button>
                 </Link>
