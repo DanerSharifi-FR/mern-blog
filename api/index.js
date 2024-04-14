b2c6dca76594af8281a0e141cc982a28e052e5b8
Daner SHARIFI <daner.sharifi.official@gmail.com>
2024-04-14 03:13:02 +0200

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "nodemon";
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(r => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(e => {
+        console.log('Error while connecting to MongoDB');
+    });
 
 const app = express();
 
