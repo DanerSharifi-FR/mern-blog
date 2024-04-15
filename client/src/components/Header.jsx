fddd6397bfbf71e347a6a5767e5baecc325d58fd
Daner SHARIFI <daner.sharifi.official@gmail.com>
2024-04-15 21:19:58 +0200

    Complete sign up page's UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = function (props) {
     const path = useLocation().pathname;
     return (
         <Navbar className="border-b-2">
-            <Link to="/">
+            <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
             <span
                 className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
                 Daner's
